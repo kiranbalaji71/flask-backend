93fbf7a9704a98177bcd013dd0055ec1911f75ad
kiranbalaji71 <kiran19701@gmail.com>
2025-09-29 21:25:28 +0530

    chore: setup the database

--- DAY01_Flask_Basics/config.py
+++ DAY01_Flask_Basics/config.py
@@ -5,7 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/Kiran Balaji/Download/Flask_POC/pydatabase.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/Kiran Balaji/Download/flask-backend/DAY01_Flask_Basics/pydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Kiran Balaji/Desktop/Project/flask-backend/DAY01_Flask_Basics/pydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
